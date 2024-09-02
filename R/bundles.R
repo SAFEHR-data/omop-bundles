9892d696d80df6184ae3a5a5fe052ef5ba6ebb95
Stef Piatek <s.piatek@ucl.ac.uk>
2024-09-02 18:15:20 +0100

    Create minimal R package (#14)
    * Create R package
    
    * Add in existing bundles
    
    * Add minimal functionality for bundle usage
    
    * Add in CI checks

---
+++ R/bundles.R
@@ -0,0 +1,63 @@
+#' @importFrom dplyr tibble mutate
+#' @importFrom purrr map_dfr
+#' @importFrom readr read_csv
+
+#' @title Get available bundles for a version
+#'
+#' @description If a bundle has multiple names, then the id will be duplicated across rows
+#'
+#' @param version Requested version, if not defined, the latest will be used
+#' @return dataframe that contains a "concept_name", "version" and a "domain" column for each available concept
+#' @export
+#' @examples
+#' available_bundles()
+#' available_bundles("0.1")
+available_bundles <- function(version = "latest") {
+  raw_dir <- .get_raw_dir(version = version)
+  directories <- list.dirs(raw_dir, full.names = TRUE)
+  domain_directories <- directories[directories != raw_dir]
+
+  purrr::map_dfr(domain_directories, .build_concepts_from_directory) |>
+    mutate(version = version)
+}
+
+.get_raw_dir <- function(version, ...) {
+  if (version != "latest") warning("Versioning not yet implemented, using version = 'latest'")
+
+  system.file("data-raw", ..., package = "omopbundles", mustWork = TRUE)
+}
+
+.build_concepts_from_directory <- function(directory) {
+  concept_files <- list.files(directory)
+  concept_name <- NULL
+
+  dplyr::tibble(
+    id = concept_files,
+    concept_name = concept_files,
+    domain = basename(directory)
+  ) |>
+    dplyr::mutate(concept_name = sub("\\.csv$", "", concept_name)) |>
+    dplyr::mutate(concept_name = gsub("_", " ", concept_name))
+}
+
+
+#' @title Get concepts for a a single bundle row
+#'
+#' @description Retrieves concept data for a specific bundle.
+#'
+#' @param domain The domain of the bundle.
+#' @param id The ID of the bundle.
+#' @param version The version of the bundle. Default is "latest".
+#' @return A data frame with the concept data.
+#' @export
+#' @examples
+#' # Usage with available_bundles, from a single row
+#' smoking_info <- available_bundles() |> dplyr::filter(concept_name == "smoking")
+#' concept_by_bundle(domain = smoking_info$domain, id = smoking_info$id, version = smoking_info$version)
+#' # Using if you know the details directly
+#' concept_by_bundle(domain = "observation", id = "smoking.csv")
+concept_by_bundle <- function(domain, id, version = "latest") {
+  .get_raw_dir(version = version, domain, id) |>
+    readr::read_csv(show_col_types = FALSE) |>
+    dplyr::mutate(domain = domain)
+}
---
+++ tests/testthat/test-bundles.R
@@ -0,0 +1,40 @@
+library(dplyr)
+library(omopbundles)
+library(testthat)
+
+
+test_that("available_bundles isn't empty and have correct columns", {
+  result <- omopbundles::available_bundles()
+  expect_true(nrow(result) > 0, info = "The dataframe should not be empty")
+  hiv_ab <- filter(result, concept_name == "antibodies to hiv")
+  expect_equal(hiv_ab$version, "latest")
+  expect_equal(hiv_ab$id, "antibodies_to_hiv.csv")
+  expect_equal(hiv_ab$domain, "measurement")
+
+})
+
+test_that("Smoking exists as an observation", {
+  result <- available_bundles() |>
+    dplyr::filter(concept_name == "smoking")
+
+  expect_true(nrow(result) == 1, info = "Smoking should only exist as a single row")
+  expect_equal(result$domain, "observation")
+})
+
+test_that("Concept by bundle works with character values", {
+  smoking_concepts <- omopbundles::concept_by_bundle(domain = "observation", id = "smoking.csv")
+  expect_true(nrow(smoking_concepts) > 1, info = "Smoking should have multiple concepts")
+  expect_false(any(is.na(smoking_concepts$concept_id)), info = "Concept ids should not be NA")
+  expect_true(all(smoking_concepts$domain == "observation"), info = "Domain should be set correctly")
+})
+
+test_that("Available bundles and concept_by_bundle play nicely together", {
+  smoking_bundle <- available_bundles() |>
+    dplyr::filter(concept_name == "smoking")
+
+  smoking_concepts <- concept_by_bundle(smoking_bundle$domain, smoking_bundle$id)
+
+  expect_true(nrow(smoking_concepts) > 1, info = "Smoking should have multiple concepts")
+  expect_false(any(is.na(smoking_concepts$concept_id)), info = "Concept ids should not be NA")
+  expect_true(all(smoking_concepts$domain == "observation"), info = "Domain should be set correctly")
+})
