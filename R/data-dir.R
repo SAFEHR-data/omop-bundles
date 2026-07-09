bd984bcbc0440bd9f23bce2e6f7816059977e9e6
Stef Piatek <s.piatek@ucl.ac.uk>
2026-07-09 13:51:12 +0100

    Add data dir helper function

---
+++ R/data-dir.R
@@ -0,0 +1,15 @@
+#' @title Get data-raw directory path
+#'
+#' @description Returns the path to the data-raw directory. This function can
+#'   be mocked in tests to use a temporary directory.
+#'
+#' @return Character string with path to data-raw directory
+#' @keywords internal
+get_data_raw_dir <- function() {
+  data_dir <- system.file("data-raw", package = "omopbundles")
+
+  if (!dir.exists(data_dir)) {
+    stop("data-raw directory not found. Package may need to be reinstalled.")
+  }
+  data_dir
+}
