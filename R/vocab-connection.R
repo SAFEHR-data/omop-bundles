5335337ad7ccb546276ade21766816fe5e2a742f
Stef Piatek <s.piatek@ucl.ac.uk>
2026-07-09 16:55:58 +0100

    Update package

--- R/vocab-connection.R
+++ R/vocab-connection.R
@@ -16,7 +16,11 @@
 #'
 #' @examples
 #' # CDM connection for OMOP
-#' # cdm <- CDMConnector::cdmFromCon(con, cdmName = "eunomia", cdmSchema = "main", writeSchema = "main")
+#' # cdm <- CDMConnector::cdmFromCon(con, 
+#' #   cdmName = "eunomia", 
+#' #   cdmSchema = "main",
+#' #   writeSchema = "main"
+#' # )
 #' # vocab_conn <- create_vocab_connection(
 #' #   connection = cdm,
 #' #   connection_type = "OMOP"
